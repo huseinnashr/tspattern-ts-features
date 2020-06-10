83b2eb8534eedcce5a488adf399258a406539f37
Husein <nvdhunter007@gmail.com>
2020-06-10 21:22:14 +0700

    contstraining generic

--- generic.ts
+++ generic.ts
@@ -43,3 +43,27 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything<string>(["a", "b", "c"]);
+
+class Car implements Printable {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House implements Printable {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHouseOrHouse<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHouseOrHouse([new House(), new Car()]);
