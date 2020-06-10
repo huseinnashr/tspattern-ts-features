b00b478bea99376debc96c72d7ac717a746a8ace
Husein <nvdhunter007@gmail.com>
2020-06-10 21:15:29 +0700

    generic to generalize type in function

--- generic.ts
+++ generic.ts
@@ -22,4 +22,24 @@ class ArrayOfAnything<T> {
   }
 }
 
-new ArrayOfAnything<string>(["a", "b", "c"]);
+const arr = new ArrayOfAnything(["a", "b", "c"]);
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c"]);
