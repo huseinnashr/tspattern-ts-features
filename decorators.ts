28dbd5dea23a92e3f24af883aec512be89e9e9bf
Husein <nvdhunter007@gmail.com>
2020-06-12 16:27:32 +0700

    try out decorator for logging an error

---
+++ decorators.ts
@@ -0,0 +1,27 @@
+class Boat {
+  color: string = "red";
+
+  get formattedColort(): string {
+    return `This boat color is ${this.color}`;
+  }
+
+  @logError
+  pilot(): void {
+    throw new Error();
+    console.log("swish");
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log("Oops, boat was sunk");
+    }
+  };
+}
+
+new Boat().pilot();
