5f53424077c87ca27dc8c42a3cb99396fb816f97
Husein <nvdhunter007@gmail.com>
2020-06-12 16:39:12 +0700

    decorator on method param & class constructor

--- decorators.ts
+++ decorators.ts
@@ -1,15 +1,36 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = "red";
 
+  @testDecorator
   get formattedColort(): string {
     return `This boat color is ${this.color}`;
   }
 
   @logError("Something bad!")
-  pilot(): void {
-    throw new Error();
-    console.log("swish");
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === "fast") {
+      console.log("swish");
+    } else {
+      console.log("nothing");
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log("Key: ", key);
 }
 
 function logError(errorMessage: string) {
@@ -25,5 +46,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
