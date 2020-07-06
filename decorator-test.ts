4f17059f0665b88c0b3377321ee3a6158980a7fa
Husein <nvdhunter007@gmail.com>
2020-07-07 03:02:09 +0700

    model binding example

--- decorator-test.ts
+++ decorator-test.ts
@@ -1,12 +1,19 @@
+import "reflect-metadata";
+
+enum MetadataKeys {
+  METHOD = "METHOD",
+  BIND = "BIND",
+}
+
 const planeClassD: ClassDecorator = (constructor: Function) => {
-  console.log("Class:", constructor);
+  // console.log("Class:", constructor);
 };
 
 const planePropertyD: PropertyDecorator = (
   target: any,
   key: string | symbol
 ): void => {
-  console.log("Property: ", "Key: ", key);
+  // console.log("Property: ", "Key: ", key);
 };
 
 const planeMethodD: MethodDecorator = (
@@ -14,7 +21,7 @@ const planeMethodD: MethodDecorator = (
   key: string | symbol,
   desc: PropertyDescriptor
 ): void => {
-  console.log("Method: ", "Key: ", key);
+  // console.log("Method: ", "Key: ", key);
 };
 
 const planeParamD: ParameterDecorator = (
@@ -22,7 +29,7 @@ const planeParamD: ParameterDecorator = (
   key: string | symbol,
   index: number
 ): void => {
-  console.log("Parameter:", target, key, index);
+  // console.log("Parameter:", target, key, index);
 };
 
 @planeClassD
@@ -31,23 +38,90 @@ class Plane {
   color: string = "red";
 
   @planePropertyD
-  stick: Stick;
+  stick: StickService;
 
-  constructor(@parameterDecorator private size: number) {
-    this.stick = new Stick();
+  constructor(@planeParamD private size: number) {
+    this.stick = new StickService();
   }
 
   @planeMethodD
-  pilot(@parameterDecorator speed: string, generateWake: boolean): void {}
+  pilot(@planeParamD speed: string, generateWake: boolean): void {}
 }
 
-const StickMethodD: MethodDecorator = (
+class StickModel {
+  constructor(public id: number) {}
+}
+class BarModel {
+  constructor(public id: number) {}
+}
+
+const RegisterBind: MethodDecorator = (
   target: any,
   methodKey: string | symbol,
   desc: PropertyDescriptor
-): void => {};
+) => {
+  const originalMethod = desc.value;
 
-class Stick {
-  @StickMethodD
-  rotate() {}
+  const binds = Reflect.getMetadata(MetadataKeys.METHOD, target, methodKey);
+
+  desc.value = function (...args: any[]) {
+    for (let bind of binds) {
+      const { paramIndex, model } = bind;
+      const resolver = Reflect.getMetadata(MetadataKeys.METHOD, model);
+      args[paramIndex] = resolver(args[paramIndex]);
+    }
+
+    return originalMethod.apply(this, args);
+  };
+
+  return desc;
+};
+
+const Bind = (model: Function): ParameterDecorator => (
+  target: any,
+  methodKey: string | symbol,
+  paramIndex: number
+): void => {
+  const binds =
+    Reflect.getMetadata(MetadataKeys.METHOD, target, methodKey) || [];
+  binds.push({ paramIndex, model });
+  Reflect.defineMetadata(MetadataKeys.METHOD, binds, target, methodKey);
+};
+
+const Resolver = (model: Function): MethodDecorator => (
+  target: any,
+  methodKey: string | symbol,
+  desc: PropertyDescriptor
+): void => {
+  // console.log("StickMethodD:", target, methodKey, desc.value);
+  Reflect.defineMetadata(MetadataKeys.METHOD, target[methodKey], model);
+};
+
+class StickService {
+  @Resolver(StickModel)
+  rotate(id: number) {
+    console.log("Resolving Stick");
+    return new StickModel(id);
+  }
 }
+
+class BarService {
+  @Resolver(BarModel)
+  dive(id: number) {
+    console.log("Resolving Bar");
+    return new StickModel(id);
+  }
+}
+
+class Controller {
+  constructor() {}
+
+  @RegisterBind
+  get(@Bind(StickModel) stick: StickModel, @Bind(BarModel) bar: BarModel) {
+    console.log(stick);
+    console.log(bar);
+    return stick;
+  }
+}
+
+new Controller().get(21 as any, 23 as any);
